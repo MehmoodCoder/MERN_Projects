882025b0ecef403f8576d759d7408bfa25943af5
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-05 12:45:12 +0500

    Import all componets and packages & create routes of all components

--- CRUD_Project/frontend/src/App.jsx
+++ CRUD_Project/frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css'
+import { BrowserRouter, Router, Route } from "react-router-dom";
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+import User from './components/User';
+import CreateUser from './components/CreateUser';
+import UpdateUser from './components/UpdateUser';
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Router>
+          <Route path='/' element={<User/>}/>
+          <Route path='/create' element={<CreateUser/>}/>
+          <Route path='/update' element={<UpdateUser/>}/>
+        </Router>
+      </BrowserRouter>
     </>
   )
 }
