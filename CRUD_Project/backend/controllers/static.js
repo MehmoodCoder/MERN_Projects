b9b5250e858767fee0623fff5ba6bb323a2732e7
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-07 10:15:28 +0500

    Add the functionality of getuser route

--- CRUD_Project/backend/controllers/static.js
+++ CRUD_Project/backend/controllers/static.js
@@ -1,8 +1,14 @@
 import UserModel from "../models/user.js";
 
 export async function HomeUI(req, res) {
-    UserModel.find({})
-    .then(users => res.json(users))
-    .catch(e => res.json(e))
+  UserModel.find({})
+    .then((users) => res.json(users))
+    .catch((e) => res.json(e));
 }
 
+export async function GetUser(req, res) {
+  const id = req.params?.id;
+  UserModel.findById( id )
+    .then((users) => res.json(users))
+    .catch((e) => res.json(`Error`, e));
+}
