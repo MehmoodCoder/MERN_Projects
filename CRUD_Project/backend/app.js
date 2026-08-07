bde2512ead2328ac1c4fd664dead1a6dd1ef7758
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-07 08:36:14 +0500

    Resolve all errors and create first post route & connectDB

--- CRUD_Project/backend/app.js
+++ CRUD_Project/backend/app.js
@@ -5,7 +5,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-import connectDB from "./connection.js";
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+import StaticURLRoute from "./routes/static.js";
+import UserRoute from "./routes/user.js";
+
+import ConnectDB from "./connection.js";
 
 app.use(async (req, res, next) => {
   try {
@@ -17,11 +23,11 @@ app.use(async (req, res, next) => {
   }
 });
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.use(cors());
 app.use(express.json());
+
+app.use("/", StaticURLRoute);
+app.use("/create", UserRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
