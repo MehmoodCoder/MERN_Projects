2cb151d6d8e4feb426d05896a9a098416e276654
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-07 15:40:00 +0500

    Create delete route

--- CRUD_Project/backend/routes/static.js
+++ CRUD_Project/backend/routes/static.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { HomeUI, GetUser, UpdateUser } from "../controllers/static.js";
+import { HomeUI, GetUser, UpdateUser, DeleteUser } from "../controllers/static.js";
 
 const router = express.Router()
 
@@ -8,5 +8,6 @@ const router = express.Router()
 router.get('/', HomeUI)
 router.get('/getuser/:id', GetUser)
 router.put('/update/:id', UpdateUser)
+router.delete('/delete/:id', DeleteUser)
 
 export default router
