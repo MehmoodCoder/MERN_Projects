62ea4bf1a926d817946079c72bd734bbbf5ae3a9
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-07 21:06:49 +0500

    Correct routes

--- CRUD_Project/backend/app.js
+++ CRUD_Project/backend/app.js
@@ -1,31 +1,33 @@
 import express from "express";
 import cors from "cors";
-
 import dotenv from "dotenv";
+import StaticURLRoute from "./routes/static.js";
+import UserRoute from "./routes/user.js";
+import ConnectDB from "./connection.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-import StaticURLRoute from "./routes/static.js";
-import UserRoute from "./routes/user.js";
-
-import ConnectDB from "./connection.js";
-
 app.use(cors());
 app.use(express.json());
-
-app.use("/", StaticURLRoute);
-app.use("/create", UserRoute);
 
 ConnectDB(process.env.MONGO_URL)
   .then(() => {
     console.log("Database Connected Successfully");
-    app.listen(PORT, () => {
-      console.log(`Server is running on PORT ${PORT}`);
-    });
   })
   .catch((error) => {
     console.error("Database connection failure:", error);
   });
+
+app.use("/api", StaticURLRoute);
+app.use("/api/create", UserRoute);
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+  });
+}
+
+export default app;
