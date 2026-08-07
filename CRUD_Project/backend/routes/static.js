cd17ef1ae83f0874d8bebd3c04fc90db8ff672d3
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-07 21:35:21 +0500

    Add CreateNewUser route here from extra file

--- CRUD_Project/backend/routes/static.js
+++ CRUD_Project/backend/routes/static.js
@@ -1,11 +1,12 @@
 import express from 'express'
 
-import { HomeUI, GetUser, UpdateUser, DeleteUser } from "../controllers/static.js";
+import { HomeUI, CreateNewUser, GetUser, UpdateUser, DeleteUser } from "../controllers/static.js";
 
 const router = express.Router()
 
 
 router.get('/', HomeUI)
+router.post('/', CreateNewUser);
 router.get('/getuser/:id', GetUser)
 router.put('/update/:id', UpdateUser)
 router.delete('/delete/:id', DeleteUser)
