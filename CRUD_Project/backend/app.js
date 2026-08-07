d741569abc941398e57136225e2ae3fda4d119e6
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-07 08:59:15 +0500

    Resolve error which is occured by the sequence of execution flow Correct it

--- CRUD_Project/backend/app.js
+++ CRUD_Project/backend/app.js
@@ -13,22 +13,19 @@ import UserRoute from "./routes/user.js";
 
 import ConnectDB from "./connection.js";
 
-app.use(async (req, res, next) => {
-  try {
-    await ConnectDB(process.env.MONGO_URL);
-    next();
-  } catch (error) {
-    console.error("Database connection failure:", error);
-    res.status(500).send("Database Connection Failed");
-  }
-});
-
 app.use(cors());
 app.use(express.json());
 
 app.use("/", StaticURLRoute);
 app.use("/create", UserRoute);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`);
-});
+ConnectDB(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database Connected Successfully");
+    app.listen(PORT, () => {
+      console.log(`Server is running on PORT ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection failure:", error);
+  });
