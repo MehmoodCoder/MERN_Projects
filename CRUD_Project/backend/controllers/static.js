52adc5668d4e023bed7a4460400cf89b91fc31b2
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-07 13:16:38 +0500

    Add the functionality of update route

--- CRUD_Project/backend/controllers/static.js
+++ CRUD_Project/backend/controllers/static.js
@@ -8,7 +8,18 @@ export async function HomeUI(req, res) {
 
 export async function GetUser(req, res) {
   const id = req.params?.id;
-  UserModel.findById( id )
+  UserModel.findById(id)
     .then((users) => res.json(users))
-    .catch((e) => res.json(`Error`, e));
+    .catch((e) => res.json(`Error :`, e));
+}
+
+export async function UpdateUser(req, res) {
+  const id = req.params?.id;
+  UserModel.findByIdAndUpdate(id, {
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age,
+  })
+    .then((users) => res.json(users))
+    .catch((e) => res.json(`Error :`, e));
 }
