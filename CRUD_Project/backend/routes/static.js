6e6a337133220b34061ed38f04b0da91cf6b7289
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-07 13:16:07 +0500

    Add new update route

--- CRUD_Project/backend/routes/static.js
+++ CRUD_Project/backend/routes/static.js
@@ -1,11 +1,12 @@
 import express from 'express'
 
-import { HomeUI, GetUser } from "../controllers/static.js";
+import { HomeUI, GetUser, UpdateUser } from "../controllers/static.js";
 
 const router = express.Router()
 
 
 router.get('/', HomeUI)
 router.get('/getuser/:id', GetUser)
+router.put('/update/:id', UpdateUser)
 
 export default router
