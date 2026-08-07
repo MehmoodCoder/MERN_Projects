79969651fc808054d1fc80da4f0bde39e88544b7
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-07 21:32:34 +0500

    Shift createUser functionality to static route

--- CRUD_Project/backend/routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-import { CreateNewUser } from "../controllers/user.js";
-
-const router = express.Router()
-
-router.post('/', CreateNewUser);
-
-export default router
