d023e545f9cf1b5eacf84440b63ea0ed5f11b5ab
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-26 06:24:51 +0500

    Convert code from .jsx to .tsx

--- CRUD_Project/frontend/src/App.tsx
+++ CRUD_Project/frontend/src/App.tsx
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import User from './components/User';
 import CreateUser from './components/CreateUser';
 import UpdateUser from './components/UpdateUser';
-
 function App() {
 
   return (
